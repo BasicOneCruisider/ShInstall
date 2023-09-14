50328e21e1c917f38b0311381d9f2d52d526c055
FrancisNtahimpera <Ntahimperafrancis@gmail.com>
2023-09-14 21:59:49 +0200

    Create virtualbox.sh

---
+++ virtualbox.sh
@@ -0,0 +1,26 @@
+# add repository key 
+#Download
+curl https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor > oracle_vbox_2016.gpg
+curl https://www.virtualbox.org/download/oracle_vbox.asc | gpg --dearmor > oracle_vbox.gpg
+
+#Install on system
+sudo install -o root -g root -m 644 oracle_vbox_2016.gpg /etc/apt/trusted.gpg.d/
+sudo install -o root -g root -m 644 oracle_vbox.gpg /etc/apt/trusted.gpg.d/
+
+#Once the system is updated and Repository key imported, you can add VirtualBox
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+
+sudo apt update
+sudo apt install linux-headers-$(uname -r) dkms
+
+#install vb
+sudo apt install virtualbox-7.0
+
+#Download VirtualBox Extension Pack.
+cd ~/
+VER=$(curl -s https://download.virtualbox.org/virtualbox/LATEST.TXT)
+wget https://download.virtualbox.org/virtualbox/$VER/Oracle_VM_VirtualBox_Extension_Pack-$VER.vbox-extpack
+
+#Once the file is downloaded, install the extension pack using CLI:
+sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-*.vbox-extpack
+
