4bad5d3c58898f8e5e0c289f78a639d15323af6d
FrancisNtahimpera <Ntahimperafrancis@gmail.com>
2024-10-25 00:17:13 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,19 +1,20 @@
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update
+
 # install docker
-sudo apt-get install docker docker-compose-plugin
+sudo apt install docker-ce docker-compose-plugin
+
+sudo usermod -aG ${USER}
+su - ${USER}
 
 # enable and start
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo systemctl status docker
