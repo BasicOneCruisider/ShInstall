6070be346c02ac5d7f703b4f6fd23968b2e5f3dc
BasicOne <Ntahimperafrancis@gmail.com>
2025-03-14 15:14:28 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,19 +2,28 @@
 sudo apt-get update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Ajouter la clé GPG de Docker :
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+# Ajoutez le dépôt de Docker :
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
 
 # install docker
-sudo apt install docker-ce docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG ${USER}
-su - ${USER}
+# Se déconnecter et se reconnecter pour appliquer les modifications de groupe :
+newgrp docker
+
 
 # enable and start
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo systemctl status docker
+
+
+# Vérification de l'installation
+
+docker run hello-world
